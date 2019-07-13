e5e716c0d46d6f37e9eb094cab69a8a8e28f7ce5
Therese Brown <browntm96@gmail.com>
2019-07-13 11:26:43 -0700

    Create event listeners for <th> and the button, alternate X and O, reset board when button clicked, restart back at X

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,24 @@
+var turn = 'X';
 
+var thList = document.querySelectorAll('th');
+
+thList.forEach((th) => {
+  th.addEventListener('click', event => {
+    th.innerHTML = `${turn}`;
+    if (turn === 'X') {
+      turn = 'O';
+    } else {
+      turn = 'X';
+    }
+  });
+});
+
+var button = document.querySelector('button');
+
+button.addEventListener('click', event => {
+  thList.forEach((th) => {
+    th.innerHTML = ''
+  });
+  turn = 'X';
+});
 
