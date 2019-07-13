130f138e01dc2007522b625d69aeb3e93c79d9ad
Therese Brown <browntm96@gmail.com>
2019-07-13 11:51:28 -0700

    Complete BMR and some Advanced Content

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -4,7 +4,26 @@ var thList = document.querySelectorAll('th');
 
 thList.forEach((th) => {
   th.addEventListener('click', event => {
-    th.innerHTML = `${turn}`;
+    if (th.innerHTML === '') {
+      th.innerHTML = `${turn}`;
+      if (thList[0].innerHTML === thList[1].innerHTML && thList[1].innerHTML === thList[2].innerHTML && thList[0].innerHTML !== '') {
+        alert(`${thList[0].innerHTML} won!`);
+      } else if (thList[0].innerHTML === thList[4].innerHTML && thList[4].innerHTML === thList[8].innerHTML && thList[0].innerHTML !== '') {
+        alert(`${thList[0].innerHTML} won!`);
+      } else if (thList[0].innerHTML === thList[3].innerHTML && thList[3].innerHTML === thList[6].innerHTML && thList[0].innerHTML !== '') {
+        alert(`${thList[0].innerHTML} won!`);
+      } else if (thList[1].innerHTML === thList[4].innerHTML && thList[4].innerHTML === thList[7].innerHTML && thList[1].innerHTML !== '') {
+        alert(`${thList[1].innerHTML} won!`);
+      } else if (thList[2].innerHTML === thList[5].innerHTML && thList[5].innerHTML === thList[8].innerHTML && thList[2].innerHTML !== '') {
+        alert(`${thList[2].innerHTML} won!`);
+      } else if (thList[2].innerHTML === thList[4].innerHTML && thList[4].innerHTML === thList[6].innerHTML && thList[2].innerHTML !== '') {
+        alert(`${thList[2].innerHTML} won!`);
+      } else if (thList[3].innerHTML === thList[4].innerHTML && thList[4].innerHTML === thList[5].innerHTML && thList[3].innerHTML !== '') {
+        alert(`${thList[3].innerHTML} won!`);
+      } else if (thList[6].innerHTML === thList[7].innerHTML && thList[7].innerHTML === thList[8].innerHTML && thList[6].innerHTML !== '') {
+        alert(`${thList[6].innerHTML} won!`);
+      }
+    }
     if (turn === 'X') {
       turn = 'O';
     } else {
