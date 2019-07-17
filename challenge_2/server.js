8158bdcc74cda491b91ea9594dce62ee6c79e910
Therese Brown <browntm96@gmail.com>
2019-07-16 19:40:27 -0700

    Make app use body-parser

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.use(express.static('client'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static(__dirname + '/client'));
 
 app.get('/', (req, res) => {
-  res.send('Made it');
+  res.send();
+});
+
+app.post('/', (req, res) => {
+  res.send();
 });
 
 app.listen(port, () => {
