6a98e827f1ef37aafe0cae8d1d4519a7760179f2
Therese Brown <browntm96@gmail.com>
2019-07-16 19:20:32 -0700

    Serve up index.html file using express

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.use(express.static('client'));
+
+app.get('/', (req, res) => {
+  res.send('Made it');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
