81f5fe51b544acff7cc048b895296734a6d8ee1c
Therese Brown <browntm96@gmail.com>
2019-12-17 15:43:31 -0500

    Work in progress

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,17 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const fs = require('fs');
+const helper = require('./client/helper.js');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/client'));
 
-app.get('/', (req, res) => {
-  res.send();
+const storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, 'samples/');
+  },
+  filename: function(req, file, callback) {
+    callback(null, file.fieldname);
+  }
 });
 
-app.post('/', (req, res) => {
-  res.send();
+const upload = multer({ storage });
+
+app.get('/', (req, res) => {
+  res.status(200);
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/download', (req, res) => {
+  res.download(__dirname + '/samples/csv_report.csv');
+});
+
+app.post('/upload_json', upload.single('jsonFile'), (req, res, next) => {
+  var file = req.file;
+  if (!file) {
+    var error = new Error().status(400);
+    return next(error);
+  } else {
+    res.send(file);
+  }
+
+  fs.readFile(__dirname + '/samples/sales_report.json', (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      fs.writeFile(__dirname + '/samples/csv_report.csv', helper.convertToCSV(JSON.parse(data)), (err)=> {
+        if (err) {
+          throw err;
+        } else {
+          console.log('CSV saved successfully');
+        }
+      });
+    }
+  });
 });
 
 app.listen(port, () => {
